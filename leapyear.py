69972ccc281a958e51d3b6cf2358d82f5e8ca257
srijancse <srijanagarwal.cse@gmail.com>
2016-04-02 17:02:54 +0530

    First commit

---
+++ leapyear.py
@@ -0,0 +1,14 @@
+n = input('Which year do you want to know whether is leap? :')
+
+def leapyear(n):
+
+	if n % 400 == 0 :
+		return True
+	if n % 100 == 0 :
+		return True
+	if n % 4 == 0 :
+		return True 
+	else :
+		return False
+
+print leapyear(n)
